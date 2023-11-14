feed86658a6a81f169cd4cde9ff659dfd9bae0f5
thomas.caron <tc.caron91@gmail.com>
2023-11-14 14:44:58 +0100

    Average padding color excluding the Lego brick

--- lego_detector.py
+++ lego_detector.py
@@ -4,22 +4,16 @@ import uuid
 import numpy as np
 
 
-def whiten_lego_piece(image):
-  """
-  Increase the brightness of the Lego piece to make it appear whiter.
-  """
-  # Convert to HSV for easier brightness adjustment
-  hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-  h, s, v = cv2.split(hsv)
-
-  # Increase brightness
-  lim = 255 - 50
-  v[v > lim] = 255
-  v[v <= lim] += 50
-
-  final_hsv = cv2.merge((h, s, v))
-  image = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
-  return image
+# Calculate the average color of pixels darker than [50, 50, 50]
+def average_dark_color(image):
+  dark_pixels = []
+  for row in image:
+    for pixel in row:
+      if all(pixel < [50, 50, 50]):  # Check if the pixel is darker than the threshold
+        dark_pixels.append(pixel)
+  if not dark_pixels:
+    return [25, 25, 25]  # Fallback color if no dark pixels are found
+  return np.mean(dark_pixels, axis=0)
 
 
 def process_image(user_image_path):
@@ -36,38 +30,83 @@ def process_image(user_image_path):
   # Gaussian blur to smooth out pixels
   diff_gray_blur = cv2.GaussianBlur(diff_gray, (5, 5), 0)
 
-  # Find threshold to convert to binary image using Otsu's method
+  # Find threshold to convert to binary image using Otsu's meth od
   ret, img_tresh = cv2.threshold(diff_gray_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
   # Find contours
   arr_cnt, a2 = cv2.findContours(img_tresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-  # Dimensions of the image
+  # Copy image to avoid overwriting
+  img_with_allcontours = img_example.copy()
+  cv2.drawContours(img_with_allcontours, arr_cnt, -1, (0, 255, 0), 3)
+
+  # DISCARD Lego blocks that are not considered to be a block (valid contours)
+
+  # Dimensions de l'image
   height, width, channels = img_example.shape
 
   validcontours = []
   contour_index = -1
 
-  # Iterate over each contour
+  # Iterer sur chaque contour
   for i in arr_cnt:
-    contour_index += 1
+
+    contour_index = contour_index + 1
     ca = cv2.contourArea(i)
 
-    # Calculate aspect ratio
+    # Calcul du ratio W/H
     x, y, w, h = cv2.boundingRect(i)
     aspect_ratio = float(w) / h
 
-    # Flag as noise if the contour is on the edge of the image
-    edge_noise = x == 0 or y == 0 or (x + w) == width or (y + h) == height
+    # Flag en tant que bruit si le contour est sur le bord de l'image
+    # Les contours qui sont sur le bord de l'image sont generalement du bruit
+    edge_noise = False
+    if x == 0:
+      edge_noise = True
+    if y == 0:
+      edge_noise = True
+    # Si l'objet est sur le bord de l'image, on le considere comme du bruit
+    if (x + w) == width:
+      edge_noise = True
+    if (y + h) == height:
+      edge_noise = True
 
-    # Keep contour if area is large enough and not on the edge
-    if ca > 1300 and aspect_ratio <= 6 and not edge_noise:
-      validcontours.append(contour_index)
+    # SUPPRIMER si l'aire est trop petite
+    if ca > 1300:
+      if aspect_ratio <= 6:
+        # SUPPRIMER si le contour est sur le bord de l'image
+        if not edge_noise:
+          validcontours.append(contour_index)
+
+  # Copier l'image originale
+  img_withcontours = img_example.copy()
+
+  # Afficher le nombre de contours valides
+  if len(validcontours) > 1:
+    print("There is more than 1 object in the picture")
+  else:
+    if len(validcontours) == 1:
+      print("One object detected")
+    else:
+      print("No objects detected")
+
+  for i in validcontours:
+    cv2.drawContours(img_withcontours, arr_cnt, i, (0, 255, 0), 3)
+
+  # Afficher les bounding boxes
+  img_withrectangle = img_example.copy()
+  for i in validcontours:
+    x, y, w, h = cv2.boundingRect(arr_cnt[i])
+    cv2.rectangle(img_withrectangle, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+  # Save each brick in a separate file
+  img_withrectangle = img_example.copy()
 
   # Check output folder
   if not os.path.exists('output'):
     os.makedirs('output')
 
+  # Iterer sur chaque contour
   bricks_data = []
   for i in validcontours:
     x, y, w, h = cv2.boundingRect(arr_cnt[i])
@@ -76,27 +115,32 @@ def process_image(user_image_path):
       "id": i,
       "position": {"x": x, "y": y},
       "size": {"width": w, "height": h},
-      "image_path": f"output/{unique_filename}"
+      "image_path": f"http://127.0.0.1:5000/output/{unique_filename}"
     }
     bricks_data.append(brick_info)
+    cv2.rectangle(img_withrectangle, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # Crop image
     crop_img = img_example[y:y + h, x:x + w]
 
-    # Whiten the Lego piece
-    crop_img = whiten_lego_piece(crop_img)
+    # Assuming 'img' is your image
+    avg_color = average_dark_color(crop_img)
+    print(avg_color)
 
-    # Create a black background
-    black_background = np.zeros_like(crop_img)
+    # Force the image to be a square, fill the rest with 25, 25, 25
+    if w > h:
+      # Add padding to the top and bottom
+      pad = int((w - h) / 2)
+      crop_img = cv2.copyMakeBorder(crop_img, pad, pad, 0, 0, cv2.BORDER_CONSTANT, value=avg_color)
+    else:
+      # Add padding to the left and right
+      pad = int((h - w) / 2)
+      crop_img = cv2.copyMakeBorder(crop_img, 0, 0, pad, pad, cv2.BORDER_CONSTANT, value=avg_color)
 
-    # Create a mask from the cropped image
-    gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-    _, mask = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
+    # Make the image grey
+    crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
 
-    # Combine the cropped image with the black background
-    black_background[mask == 255] = crop_img[mask == 255]
-
-    # Save the image with a black background and the lego piece colored whiter
-    cv2.imwrite(f"output/{unique_filename}", black_background)
+    # Save the cropped image with the unique filename
+    cv2.imwrite(f"output/{unique_filename}", crop_img)
 
   return bricks_data
